f6bdec8ad45b2dbd2dea0cfee16a34df09e4e1cf
Eoin Shanaghy <eoin.shanaghy@gmail.com>
2015-06-08 00:21:28 +0100

    GoL machine to create worlds of cells with neighbour awareness.

--- lib/gol_ex/cell.ex
+++ lib/gol_ex/cell.ex
@@ -5,23 +5,23 @@ defmodule Cell do
   Starts a cell process, returning the PID
   """
   def start(x, y) do
-    spawn fn -> loop(x, y, :dead, %{}) end
+    spawn fn -> loop(x, y, false, []) end
   end 
 
-  def loop(x, y, state, neighbours) do
+  def loop(x, y, state, neighbours, num_alive \\ 0) do
     receive do
-      {:state, new_state} -> loop(x, y, new_state, neighbours)
-      {:neighbour_change, neighbour, new_state} -> 
-        neighbours = %{neighbours | neighbour: new_state}
-        num_alive = Map.values(neighbours ) |> Enum.count(fn(value) -> value == :alive end)
-        loop(x, y, get_state(state, neighbours), neighbours)
+      {:setup, init_state, neighbours} -> 
+        loop(x, y, init_state, neighbours, num_alive)
+      {:neighbour_state, _neighbour, new_state} -> 
+        if new_state do num_alive_now = num_alive + 1 else num_alive_now = num_alive - 1 end
+        loop(x, y, get_state(state, num_alive_now), neighbours, num_alive_now)
     end 
   end
 
   mdef get_state do
-    :alive, neighbours_alive when neighbours_alive == 2 or neighbours_alive == 3 -> :alive
-    :alive, _ -> :dead
-    :dead, 3 -> :alive
-    :dead, _ -> :dead     
+    true, neighbours_alive when neighbours_alive == 2 or neighbours_alive == 3 -> true
+    true, _ -> false
+    false, 3 -> true
+    false, _ -> false     
   end
 end
---
+++ lib/gol_ex/machine.ex
@@ -0,0 +1,63 @@
+defmodule Machine do
+  use GenServer
+
+  @width 40
+  @height 40  
+  @tick_interval 500
+
+  def start do
+    {:ok, pid} = GenServer.start(__MODULE__, [])
+    :timer.send_interval(@tick_interval, pid, :tick)
+    {:ok, pid}
+  end
+
+  def get_world(pid) do
+    GenServer.call(pid, :get_world)
+  end
+
+  def init(_args) do
+    :random.seed(:erlang.now)
+    world = create_world(@width, @height)
+    init_world(@width, @height, world)
+    {:ok, true}
+  end
+
+  def create_world(w, h) do
+    for y <- 0..h-1 do
+      for x <- 0..w-1 do
+        create_cell(x, y)
+      end
+    end
+    |> Enum.map(&List.to_tuple/1)
+    |> List.to_tuple
+  end
+
+  defp find_neighbours(x, y, world) do
+    for (i <- x-1..x+1), (j <- y-1..y+1), 
+        (i >= 0 and i < @width and j >= 0 and j < @height and (i != x or j != y)) do
+      world |> get_cell(i,j)
+    end
+  end
+
+  defp init_world(w, h, world) do
+    for y <- 0..h-1 do
+      for x <- 0..w-1 do
+        neighbours=find_neighbours(x, y, world) 
+        alive = :random.uniform(2) == 2  
+        world |> get_cell(x,y) |> init_cell(alive, neighbours)
+      end
+    end
+  end
+
+  defp get_cell(world, x,y) do
+      world |> elem(y) |> elem(x)
+  end
+
+  defp create_cell(x, y) do
+    Cell.start(x, y)
+  end
+
+  defp init_cell(cell_pid, alive, neighbours) do
+    send cell_pid, {:setup, alive, neighbours}
+  end
+end
