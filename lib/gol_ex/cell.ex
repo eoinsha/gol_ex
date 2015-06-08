4c7e7c71350077445ddf69bbe722d4864b0c5cc3
Eoin Shanaghy <eoin.shanaghy@gmail.com>
2015-06-09 00:26:12 +0100

    Corrections based on struct based state

--- lib/gol_ex/cell.ex
+++ lib/gol_ex/cell.ex
@@ -2,25 +2,25 @@ defmodule Cell do
   import MultiDef
 
   defmodule State do
-    defstruct x: nil, y: nil, alive: false, neighbours: nil, iter: 0
+    defstruct x: nil, y: nil, alive: false, neighbours: %{}, iter: 0
   end
 
   @doc """
   Starts a cell process, returning the PID
   """
   def start(x, y) do
-    spawn fn -> loop(%State{x: x, y: y}) end
+    spawn_link fn -> loop(%State{x: x, y: y}) end
   end 
 
   def loop(state) do
-    IO.inspect {"CELL", state}
     receive do
       {:setup, alive, neighbours} -> 
-        %{state | iter: 1, alive: alive, neighbours: Enum.map(neighbours, fn(cell) -> {cell, nil} end)}
+        %{state | iter: 1, alive: alive, neighbours: Enum.map(neighbours, fn(cell) -> {cell, nil} end) |> Enum.into %{}}
           |> update_state
           |> loop
       {:neighbour_state, neighbour, alive} -> 
-        %{state | iter: state.iter + 1, neighbours: %{state.neighbours | alive: alive}} 
+        #IO.inspect{self(), "Got", alive, "from", neighbour, "neighbours", state.neighbours}
+        %{state | iter: state.iter + 1, neighbours: Map.put(state.neighbours, neighbour, alive)} 
           |> step_state
           |> loop
     end 
@@ -30,12 +30,13 @@ defmodule Cell do
     neighbour_states = 
       state.neighbours |> Map.values |> Enum.reduce([alive: 0, dead: 0, unknown: 0], fn(n, acc) -> 
         case n do
-          nil -> acc ++ [unknown: acc[:unknown] + 1]
-          true -> acc ++ [alive: acc[:alive] + 1]
-          false -> acc ++ [dead: acc[:dead] + 1]
+          nil -> Keyword.put(acc, :unknown, acc[:unknown] + 1)
+          true -> Keyword.put(acc, :alive, acc[:alive] + 1)
+          false -> Keyword.put(acc, :dead, acc[:dead] + 1)
         end 
       end)
-    
+    #IO.puts "[#{state.x},#{state.y}-#{state.alive}] unknowns- #{neighbour_states[:unknown]}, alive- #{neighbour_states[:alive]}"
+
     if neighbour_states[:unknown] == 0 do # Wait until all neighbours have sent their state
       case {state.alive, neighbour_states[:alive]} do
         {true, num_alive} when num_alive == 2 or num_alive == 3 -> state
@@ -49,7 +50,10 @@ defmodule Cell do
   end
 
   defp update_state(state) do
-    state.neighbours |> Map.keys |> Enum.each fn(neighbour) -> send neighbour, {:neighbour_state, self(), state.alive} end
+    IO.puts "#{state.x},#{state.y} -> #{state.alive}"
+    state.neighbours |> Map.keys |> Enum.each fn(neighbour) -> 
+      #IO.inspect {self(),"#{state.x},#{state.y} Sending #{state.alive} to neighbour", neighbour}
+      send neighbour, {:neighbour_state, self(), state.alive} end
     state
   end
 
